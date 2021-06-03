6cf4ec467e7b9190fdce07533145df1f3e93459a
james.warren <james.warren@digital.justice.gov.uk>
2021-06-03 11:35:09 +0100

    IN-741 update to bonds

--- generate_json_files.sh
+++ generate_json_files.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-docker-compose run --rm generate python3 app.py --local=True
-rm -r mapping_definitions
-mv app/mapping_definitions/ mapping_definitions/
+docker-compose run --rm generate python3 app/app.py --local=True
